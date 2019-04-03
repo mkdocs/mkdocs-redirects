95cb8eaf1eec17463034b215ee81b1320b333daf
Dustin Burke <burkestar@gmail.com>
2019-04-03 11:08:20 -0400

    on_post_build to write HTML file with meta redirect

--- mkdocs_redirects/plugin.py
+++ mkdocs_redirects/plugin.py
@@ -1,45 +1,49 @@
+import logging
 import os
+import textwrap
 
 from mkdocs import utils as mkdocs_utils
 from mkdocs.config import config_options, Config
 from mkdocs.plugins import BasePlugin
 from mkdocs.structure.files import File
 
+log = logging.getLogger(__name__)
+log.addFilter(mkdocs_utils.warning_filter)
+
 
 class RedirectPlugin(BasePlugin):
 
-    def __init__(self):
-        self.temp_files = []
-        super(RedirectPlugin, self).__init__()
-
-    def on_files(self, files, config, **kwargs):
+    def on_post_build(self, config, **kwargs):
         redirects = config.get('redirects', {})
-        if not redirects:
-            return files
 
         for old_page, new_page in redirects.iteritems():
-            # construct markdown file content with meta redirect
-            old_page_path = os.path.join(config['docs_dir'], '{}.md'.format(old_page))
-            new_page_path = os.path.join(config['docs_dir'], '{}.html'.format(new_page))
+            old_page_path = os.path.join(config['site_dir'], '{}.html'.format(old_page))
+            new_page_path = os.path.join(config['site_dir'], '{}.html'.format(new_page))
+
+            # check that the page being redirected to actually exists
+            if not os.path.exists(os.path.dirname(new_page_path)):
+                msg = 'Redirect does not exist for path: {}'.format(new_page_path)
+                if config.get('strict', False):
+                    raise Exception(msg)
+                else:
+                    log.warn(msg)
 
             # ensure the folder path exists, recursively for nested directories.
             if not os.path.exists(os.path.dirname(old_page_path)):
                 os.makedirs(os.path.dirname(old_page_path))
 
-            # write the file content for the old Markdown file, with a meta redirect to the new page
+            # write an HTML file in the site_dir with a meta redirect to the new page
+            # note that it will prefix the path with `/` to be relative to the site root.
             with open(old_page_path, 'w') as f:
-                f.write('redirect: {}'.format('{}.html'.format(new_page)))
-
-            # keep track of these temp markdown files so we can clean them up after the build
-            self.temp_files.append(old_page_path)
-
-            old_markdown = File(old_page_path, config['docs_dir'], config['site_dir'], config['use_directory_urls'])
-            files.append(old_markdown)
-
-        return files
-
-    def on_post_build(self, config, **kwargs):
-        for f in self.temp_files:
-            if os.path.exists(f):
-                os.unlink(f)
-        # note: does not clean up temporary directories that were created to contain these files
+                f.write(textwrap.dedent("""
+                    <!doctype html>
+                    <html lang="en" class="no-js">
+                    <head>
+                        <script>var anchor=window.location.hash.substr(1);location.href="{url}"+(anchor?"#"+anchor:"")</script>
+                        <meta http-equiv="refresh" content="0; url={url}">
+                    </head>
+                    <body>
+                    Redirecting...
+                    </body>
+                    </html>
+                """).format(url='/{}.html'.format(new_page)))
