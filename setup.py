f3d0e12c982f3b25b4b7d128eba1704b8a60ff17
Oleh Prypin <oleh@pryp.in>
2023-09-16 12:39:08 +0200

    Switch to pyproject+Hatch build and Ruff linter

--- setup.py
+++ setup.py
@@ -1,66 +1,13 @@
-"""
-Copyright 2019-2022 DataRobot, Inc. and its affiliates.
-All rights reserved.
-"""
-import os
+"""Installation using setup.py is not supported.
+Use `pip install .` instead."""
 
-from setuptools import find_packages, setup
+import sys
 
+from setuptools import setup
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+sys.exit(__doc__)
 
-
-test_requirements = [
-    'pytest',
-    'black',
-    'isort',
-    'autoflake',
-]
-
-release_requirements = [
-    'twine>=1.13.0',
-]
-
-
+# Fake reference so GitHub still considers it a real package for statistics purposes.
 setup(
-    name='mkdocs-redirects',
-    version='1.2.1',
-    description='A MkDocs plugin for dynamic page redirects to prevent broken links.',
-    long_description=read('README.md'),
-    long_description_content_type="text/markdown",
-    keywords='mkdocs redirect',
-    url='https://github.com/datarobot/mkdocs-redirects',
-    author='Dustin Burke',
-    author_email='dustin@datarobot.com',
-    license='MIT',
-    license_files=['LICENSE'],
-    python_requires='>=3.7',
-    install_requires=[
-        'mkdocs>=1.1.1',
-    ],
-    extras_require={
-        'dev': test_requirements + release_requirements,
-        'test': test_requirements,
-        'release': release_requirements,
-    },
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Information Technology',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-    ],
-    packages=find_packages(),
-    entry_points={
-        'mkdocs.plugins': [
-            'redirects = mkdocs_redirects.plugin:RedirectPlugin',
-        ]
-    },
+    name="mkdocs-redirects",
 )
