3b36c72dc50c41da5fcf9604be845648d415db0d
Oleh Prypin <oleh@pryp.in>
2023-11-04 13:35:02 +0100

    Switch to ruff's formatter and import sorting

--- mkdocs_redirects/plugin.py
+++ mkdocs_redirects/plugin.py
@@ -11,7 +11,7 @@ from mkdocs.config import config_options
 from mkdocs.plugins import BasePlugin
 from mkdocs.structure.files import File
 
-log = logging.getLogger('mkdocs.plugin.redirects')
+log = logging.getLogger("mkdocs.plugin.redirects")
 
 
 HTML_TEMPLATE = """
@@ -47,7 +47,7 @@ def write_html(site_dir, old_path, new_path):
     # Write the HTML redirect file in place of the old file
     log.debug("Creating redirect: '%s' -> '%s'", old_path, new_path)
     content = HTML_TEMPLATE.format(url=new_path)
-    with open(old_path_abs, 'w', encoding='utf-8') as f:
+    with open(old_path_abs, "w", encoding="utf-8") as f:
         f.write(content)
 
 
@@ -58,26 +58,26 @@ def get_relative_html_path(old_page, new_page, use_directory_urls):
 
     relative_path = posixpath.relpath(new_path, start=posixpath.dirname(old_path))
     if use_directory_urls:
-        relative_path = relative_path + '/'
+        relative_path = relative_path + "/"
 
     return relative_path + new_hash_fragment
 
 
 def get_html_path(path, use_directory_urls):
     """Return the HTML file path for a given markdown file"""
-    f = File(path, '', '', use_directory_urls)
-    return f.dest_path.replace(os.sep, '/')
+    f = File(path, "", "", use_directory_urls)
+    return f.dest_path.replace(os.sep, "/")
 
 
 class RedirectPlugin(BasePlugin):
     # Any options that this plugin supplies should go here.
     config_scheme = (
-        ('redirect_maps', config_options.Type(dict, default={})),  # note the trailing comma
+        ("redirect_maps", config_options.Type(dict, default={})),  # note the trailing comma
     )
 
     # Build a list of redirects on file generation
     def on_files(self, files, config, **kwargs):
-        self.redirects = self.config.get('redirect_maps', {})
+        self.redirects = self.config.get("redirect_maps", {})
 
         # Validate user-provided redirect "old files"
         for page_old in self.redirects.keys():
@@ -87,12 +87,12 @@ class RedirectPlugin(BasePlugin):
         # Build a dict of known document pages to validate against later
         self.doc_pages = {}
         for page in files.documentation_pages():  # object type: mkdocs.structure.files.File
-            self.doc_pages[page.src_path.replace(os.sep, '/')] = page
+            self.doc_pages[page.src_path.replace(os.sep, "/")] = page
 
     # Create HTML files for redirects after site dir has been built
     def on_post_build(self, config, **kwargs):
         # Determine if 'use_directory_urls' is set
-        use_directory_urls = config.get('use_directory_urls')
+        use_directory_urls = config.get("use_directory_urls")
 
         # Walk through the redirect map and write their HTML files
         for page_old, page_new in self.redirects.items():
@@ -100,7 +100,7 @@ class RedirectPlugin(BasePlugin):
             page_new_without_hash, hash = _split_hash_fragment(str(page_new))
 
             # External redirect targets are easy, just use it as the target path
-            if page_new.lower().startswith(('http://', 'https://')):
+            if page_new.lower().startswith(("http://", "https://")):
                 dest_path = page_new
 
             elif page_new_without_hash in self.doc_pages:
@@ -115,7 +115,7 @@ class RedirectPlugin(BasePlugin):
 
             # DO IT!
             write_html(
-                config['site_dir'],
+                config["site_dir"],
                 get_html_path(page_old, use_directory_urls),
                 dest_path,
             )
@@ -128,5 +128,5 @@ def _split_hash_fragment(path):
     "path/to/file#hash" => ("/path/to/file", "#hash")
     "path/to/file"      => ("/path/to/file", "")
     """
-    path, hash, after = path.partition('#')
+    path, hash, after = path.partition("#")
     return path, hash + after
